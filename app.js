6a985cbe58bb5196fb5d0c2a18e847c32620c8db
Gina Lim <zoooriiel@gmail.com>
2024-11-11 21:12:53 +0800

    Upload completed challenge

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-document.getElementById
+const iconPlus = document.querySelectorAll('.icon-plus');
+const accordionHeaders = document.querySelectorAll('.accordion-header')
+const hiddenContent = document.querySelector('.hide-content')
+const revealContent = document.querySelector('.reveal-content')
+
+
+
+accordionHeaders.forEach(accordionHeader => {
+
+    accordionHeader.addEventListener("click", () => {
+        const content = accordionHeader.nextElementSibling;
+        const iconMinus = accordionHeader.querySelector("img");
+
+        if (content.classList.contains("hide-content")){
+            content.classList.replace("hide-content", "reveal-content");
+            iconMinus.src = "assets/images/icon-minus.svg";
+        }
+        else{
+            content.classList.replace("reveal-content", "hide-content");
+            iconMinus.src = "assets/images/icon-plus.svg"
+        }
+    })
+});
